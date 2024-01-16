c8db2439e6057e74a058f73915cded179dd14a8e
w0836862@myscc.ca <arvindmee25@gmail.com>
2024-01-16 13:26:12 +0530

    Init

---
+++ src/app/data.tsx
@@ -0,0 +1,23 @@
+
+
+
+export interface User {
+    id : number
+    full_name: string
+    email: string
+    avatar : string
+}
+
+
+export const Data : User[] = [
+    { "id": 1, "full_name": "John Doe", "email": "john@example.com", "avatar": "https://avatars.githubusercontent.com/u/80080049" },
+    { "id": 2, "full_name": "Jane Smith", "email": "jane@example.com", "avatar": "https://avatars.githubusercontent.com/u/31499344" },
+    { "id": 3, "full_name": "Bob Johnson", "email": "bob@example.com", "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/478.jpg" },
+    { "id": 4, "full_name": "Alice Brown", "email": "alice@example.com", "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/924.jpg" },
+    { "id": 5, "full_name": "David Wilson", "email": "david@example.com", "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/878.jpg" },
+    { "id": 6, "full_name": "Emily Miller", "email": "emily@example.com", "avatar": "https://avatars.githubusercontent.com/u/50867488" },
+    { "id": 7, "full_name": "Michael Davis", "email": "michael@example.com", "avatar": "https://avatars.githubusercontent.com/u/5420733" },
+    { "id": 8, "full_name": "Sara Taylor", "email": "sara@example.com", "avatar": "https://avatars.githubusercontent.com/u/21540405" },
+    { "id": 9, "full_name": "Tom Anderson", "email": "tom@example.com", "avatar": "https://avatars.githubusercontent.com/u/27757492" },
+    { "id": 10, "full_name": "Laura White", "email": "laura@example.com", "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/352.jpg" }
+  ]
